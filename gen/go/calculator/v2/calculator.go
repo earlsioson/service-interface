be834008ca9cfbbbb46442419234c96e7cd0b460
Earl Sioson <earlsioson@users.noreply.github.com>
2023-06-07 09:08:33 -0400

    v2.0.4

--- gen/go/calculator/v2/calculator.go
+++ gen/go/calculator/v2/calculator.go
@@ -1,6 +1,10 @@
 package calculator
 
+<<<<<<< Updated upstream
 var Version = "2.0.3"
+=======
+var Version = "2.0.4"
+>>>>>>> Stashed changes
 
 type Calculator struct {
 	Name string
--- gen/go/job/v2/job.go
+++ gen/go/job/v2/job.go
@@ -2,7 +2,7 @@ package job
 
 import "fmt"
 
-var Version = "2.0.3"
+var Version = "2.0.4"
 
 type Job struct {
 	Name string
